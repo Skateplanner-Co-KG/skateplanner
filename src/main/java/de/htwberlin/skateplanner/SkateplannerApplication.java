7cefdef2ded9807f2918f490dfdcb0f38a19c204
TalisaW <talisa.wahle@gmail.com>
2019-11-08 12:49:49 +0100

    add missing files

---
+++ src/main/java/de/htwberlin/skateplanner/SkateplannerApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.skateplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkateplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkateplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/skateplanner/SkateplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.skateplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkateplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
