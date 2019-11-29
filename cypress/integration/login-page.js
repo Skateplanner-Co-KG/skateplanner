d6ba0f873aaf761138f0969ff6792cdbe39c12cb
Konny Decimo <konstantinzehnter@protonmail.com>
2019-11-29 13:58:19 +0100

    Small change in tests

--- cypress/integration/login-page.js
+++ cypress/integration/login-page.js
@@ -1,32 +1,92 @@
-// Context gibt Rahmen
-context('Actions', () => {
+function loginAsMember(param) {
+  cy.contains(' User Name : ').click().type('member');
+  cy.contains(' Password: ').click().type('pass');
+  cy.contains('Sign In').click();
+}
+
+function loginAsTrainer(param) {
+  cy.contains(' User Name : ').click().type('trainer');
+  cy.contains(' Password: ').click().type('pass2');
+  cy.contains('Sign In').click();
+}
+
+describe('Login-Page', function() {
   beforeEach(() => {
+    // given
     cy.visit('localhost:8080')
-    // erst Seite aufrufen
   });
 
   it('Redirect to login', function() {
-    // URL sollte nach redirect gleich login sein
+    // then
     cy.url().should('eq', 'http://localhost:8080/login')
   });
 
   it('Failed login', function() {
+    // when
     cy.contains('Sign In').click();
+    // then
     cy.url().should('contain', 'login?error')
   });
 
   it('Successfull login as member', function() {
-    cy.contains(' User Name : ').click().type('member');
-    cy.contains(' Password: ').click().type('pass');
-    cy.contains('Sign In').click();
+    // when
+    loginAsMember();
+    // then
     cy.url().should('eq', 'http://localhost:8080/')
   });
 
   it('Successfull login as trainer', function() {
-    cy.contains(' User Name : ').click().type('trainer');
-    cy.contains(' Password: ').click().type('pass2');
-    cy.contains('Sign In').click();
+    // when
+    loginAsTrainer();
+    // then
     cy.url().should('eq', 'http://localhost:8080/')
   })
-
 });
+
+describe('Navigation-Bar', function () {
+  beforeEach(() => {
+    // given
+    cy.visit('localhost:8080/login');
+    loginAsTrainer();
+  });
+
+  it('navBarElements', function () {
+    // then
+    cy.contains('About us');
+    cy.contains('Contact');
+    cy.contains('Sign Out')
+  });
+
+  it('redirections', function () {
+    // when
+    cy.contains('About us').click();
+    // then
+    cy.url().should('contain','about_us');
+    // when
+    cy.contains('Contact').click();
+    // then
+    cy.url().should('contain','contact');
+  });
+
+  it('logOut', function () {
+    // when
+    cy.contains('Sign Out').click();
+    // then
+    cy.url().should('contain', 'login')
+  })
+});
+
+describe('Planner', function () {
+  beforeEach(() => {
+    //given
+    cy.visit('localhost:8080/login');
+    loginAsTrainer();
+  })
+
+  // it('addNewEvent', function () {
+  //   cy.contains('new Event').click().type('Neues Training');
+  //   cy.contains('add-Button').click()
+  //   cy.contains('Neues Training')
+  // });
+});
+
