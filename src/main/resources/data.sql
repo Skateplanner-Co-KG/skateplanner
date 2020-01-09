9af9749b4d8f7aa7885e41b089d477983c29829b
Konny Decimo <konstantinzehnter@protonmail.com>
2020-01-09 19:19:00 +0100

    added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO users(email, username, password, role) VALUES
-('member@example.org', 'member', 'pass', 'MEMBER'),
-('trainer@example.org', 'trainer', 'pass', 'TRAINER');
+('member@example.org', 'member', 'passwortvondemmember123', 'MEMBER'),
+('trainer@example.org', 'trainer', 'passwortvondemtrainer987', 'TRAINER');
 
 INSERT INTO events(name, type, description) VALUES
 ('test1', 'meta', 'some mysterious event'),
