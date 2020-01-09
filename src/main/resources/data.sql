0294b87d7b891940d6127a0a0880f628621a492c
Konny Decimo <konstantinzehnter@protonmail.com>
2020-01-09 18:33:12 +0100

    data.sql deleted

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO users(email, username, password, role) VALUES
-('member@example.org', 'member', 'pass', 'MEMBER'),
-('trainer@example.org', 'trainer', 'pass', 'TRAINER');
-
-INSERT INTO events(name, type, description) VALUES
-('test1', 'meta', 'some mysterious event'),
-('test2', 'meta', 'another strange event');
