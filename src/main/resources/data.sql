e2f66c52a4a591a6ebea76d0482d2152624d7e49
s0566430 <s0566430@htw-berlin.de>
2020-01-10 11:46:48 +0100

    removed data.sql

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO users(email, username, password, role) VALUES
-('member@example.org', 'member', 'passwortvondemmember123', 'MEMBER'),
-('trainer@example.org', 'trainer', 'passwortvondemtrainer987', 'TRAINER');
-
-INSERT INTO events(name, type, description) VALUES
-('test1', 'meta', 'some mysterious event'),
-('test2', 'meta', 'another strange event');
