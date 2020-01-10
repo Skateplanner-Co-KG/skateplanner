00c1c81c221e98fac62e619b327a694f73652c89
Konny Decimo <konstantinzehnter@protonmail.com>
2020-01-10 13:21:59 +0100

    cypress

--- cypress/integration/event.js
+++ cypress/integration/event.js
@@ -20,21 +20,17 @@ describe('Planner', function () {
         cy.visit('localhost:8080/login');
     });
 
-    it('addNewEventAsTrainerAndDelete', function () {
+    it('addNewEventAsTrainer', function () {
         loginAsTrainer();
         cy.get('[data-cy=addEventBtn]').click();
         cy.url().should('contain', 'add_event');
-        cy.get('[data-cy=eventname]').click().type('Montagstraining');
+        cy.get('[data-cy=eventname]').click().type('testEvent');
         //cy.get('[data-cy=eventtype]').click().type('testEvent');
-        cy.get('[data-cy=timespan]').click().type('13-01-2020');
-        cy.get('[data-cy=description]').click().type('Schuhe mitbringen');
-        cy.get('[data-cy=participants]').click().type('Laura, Klara');
+        cy.get('[data-cy=timespan').click().type('15-01-2020');
+        cy.get('[data-cy=description').click().type('Schuhe mitbringen');
+        cy.get('[data-cy=participants').click().type('Laura, Klara');
         cy.get('[data-cy=addEventBtn2]').click();
-        cy.url().should('eq', 'http://localhost:8080/planner');
+        cy.url().should('eq', 'https://localhost:8080/planner');
         //cy.get('[data-cy=event]').should('contain', 'Testevent am 6.12. 14 Uhr');
-        cy.get('[data-cy=deleteEventBtn]').click();
-        cy.url().should('contain', 'delete_event');
-        //cy.get('[data-cy=deleteEventId]').click().type('')
-        // Der muss die ID aus der Tabelle lesen, damit er nach ID löschen kann
     });
 });
--- cypress/integration/registrationToLogout.js
+++ cypress/integration/registrationToLogout.js
@@ -1,7 +1,7 @@
-// describe('Registration', function() {
-//     it('registration', function () {
-//         cy.visit('localhost:8080');
-//         cy.url().should('eq', 'http://localhost:8080/login');
-//         cy.contains('')
-//     })
-// });
+describe('Registration', function() {
+    it('registration', function () {
+        cy.visit('localhost:8080');
+        cy.url().should('eq', 'http://localhost:8080/login');
+        cy.contains()
+    })
+}
