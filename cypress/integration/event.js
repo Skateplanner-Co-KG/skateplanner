5935dbbaeb9461d3cadd3730c510184107fccf4c
Konny Decimo <konstantinzehnter@protonmail.com>
2020-01-11 17:33:11 +0100

    cypress added back in

---
+++ cypress/integration/event.js
@@ -0,0 +1,37 @@
+/**
+ * Author: Konstantin
+ */
+
+function loginAsMember(param) {
+    cy.get('[data-cy=username]').click().type('member');
+    cy.get('[data-cy=password]').click().type('pass');
+    cy.get('[data-cy=signIn]').click();
+}
+
+function loginAsTrainer(param) {
+    cy.get('[data-cy=username]').click().type('trainer');
+    cy.get('[data-cy=password]').click().type('pass');
+    cy.get('[data-cy=signIn]').click();
+}
+
+describe('Planner', function () {
+    beforeEach(() => {
+        //given
+        cy.visit('localhost:8080/login');
+    });
+
+    it('addNewEventAsTrainer', function () {
+        loginAsTrainer();
+        cy.get('[data-cy=addEventBtn]').click();
+        cy.url().should('contain', 'add_event');
+        // nicht zweimal hintereinander ausführen
+        cy.get('[data-cy=eventname]').click().type('fakgufgufo');
+        //cy.get('[data-cy=eventtype]').click().type('testEvent');
+        cy.get('[data-cy=timespan').click().type('15-01-2020');
+        cy.get('[data-cy=description').click().type('Schuhe mitbringen');
+        cy.get('[data-cy=participants').click().type('Laura, Klara');
+        cy.get('[data-cy=addEventBtn2]').click();
+        cy.url().should('eq', 'http://localhost:8080/planner');
+        //cy.get('[data-cy=event]').should('contain', 'Testevent am 6.12. 14 Uhr');
+    });
+});
