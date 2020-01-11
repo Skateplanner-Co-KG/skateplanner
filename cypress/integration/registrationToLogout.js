e73c7945a556bdda8f402c62cfe481b03981ea76
Konny Decimo <konstantinzehnter@protonmail.com>
2020-01-11 17:45:49 +0100

    cypress base url added

--- cypress/integration/registrationToLogout.js
+++ cypress/integration/registrationToLogout.js
@@ -2,6 +2,6 @@ describe('Registration', function() {
     it('registration', function () {
         cy.visit('localhost:8080');
         cy.url().should('eq', 'http://localhost:8080/login');
-        cy.contains()
+        //cy.contains()
     })
-}
+});
