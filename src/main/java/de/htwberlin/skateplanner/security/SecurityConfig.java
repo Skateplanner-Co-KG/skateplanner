008db6e43c565affa53a047db91bfee0b0efa818
s0566430 <s0566430@htw-berlin.de>
2020-01-21 19:33:22 +0100

    added profiles for development and production environments

--- src/main/java/de/htwberlin/skateplanner/security/SecurityConfig.java
+++ src/main/java/de/htwberlin/skateplanner/security/SecurityConfig.java
@@ -3,11 +3,13 @@ package de.htwberlin.skateplanner.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,19 +18,19 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    public static final String ROLE_MEMBER = "MEMBER";
+    public static final String ROLE_TRAINER = "TRAINER";
     @Autowired
     UserDetailsService userDetailsService;
 
-    public static final String ROLE_MEMBER = "MEMBER";
-    public static final String ROLE_TRAINER = "TRAINER";
+    @ModelAttribute("role")
+    public static String getRoleTrainer() {
+        return ROLE_TRAINER;
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
-    }
-@ModelAttribute("role")
-    public static String getRoleTrainer() {
-        return ROLE_TRAINER;
     }
 
     @Override
@@ -43,5 +45,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin().loginPage("/login").permitAll().defaultSuccessUrl("/planner", true)
                 .and()
                 .logout().permitAll();
+    }
+
+    @Bean
+    @Profile("dev")
+    public PasswordEncoder getPasswordEncoderDev() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    @Profile("prod")
+    public PasswordEncoder getPasswordEncoderProd() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/resources/data-dev.sql
+++ src/main/resources/data-dev.sql
@@ -1,6 +1,6 @@
 INSERT INTO users(email, username, password, role) VALUES
-('member@example.org', 'member', 'pass', 'MEMBER'),
-('trainer@example.org', 'trainer', 'pass', 'TRAINER');
+('', 'member', 'pass', 'MEMBER'),
+('', 'trainer', 'pass', 'TRAINER');
 
 INSERT INTO events(name, type, description) VALUES
 ('test1', 'meta', 'some mysterious event'),
