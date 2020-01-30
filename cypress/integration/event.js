46ae88cd6c2b96cd83806aaa14cf8ec1b7677176
Konny Decimo <konstantinzehnter@protonmail.com>
2020-01-30 12:38:22 +0100

    more ui tests and error pages nicer and about us changed

--- cypress/integration/event.js
+++ cypress/integration/event.js
@@ -1,7 +1,3 @@
-/**
- * Author: Konstantin
- */
-
 function loginAsMember(param) {
     cy.get('[data-cy=username]').click().type('member');
     cy.get('[data-cy=password]').click().type('pass');
@@ -14,30 +10,46 @@ function loginAsTrainer(param) {
     cy.get('[data-cy=signIn]').click();
 }
 
+function createEvent(param) {
+    loginAsTrainer();
+    cy.get('[data-cy=addEventBtn]').click();
+    cy.url().should('contain', 'add_event');
+    cy.get('[data-cy=eventname]').click().type('Freitagstraining');
+    cy.get('[data-cy=timespan]').click().type('24-01-2020');
+    cy.get('[data-cy=description]').click().type('Schuhe mitbringen');
+    cy.get('[data-cy=addEventBtn2]').click();
+    cy.url().should('eq', 'http://localhost:8080/planner');
+    cy.get('div#cal').contains('Freitagstraining');
+}
+
+function deleteEvent(param) {
+    cy.get('[data-cy=deleteEventBtn]').click();
+    cy.url().should('contain', 'delete_event');
+    cy.get('[data-cy=deleteEventName]').click().type('Freitagstraining');
+    cy.get('[data-cy=deleteEventBtn2').click();
+
+    cy.url().should('eq', 'http://localhost:8080/planner');
+}
+
+
 describe('Planner', function () {
     beforeEach(() => {
-        //given
         cy.visit('localhost:8080/login');
     });
 
     it('addNewEventAsTrainerAndDelete', function () {
-        loginAsTrainer();
-        cy.get('[data-cy=addEventBtn]').click();
-        cy.url().should('contain', 'add_event');
-        cy.get('[data-cy=eventname]').click().type('Freitagstraining');
-        //cy.get('[data-cy=eventtype]').click().type('testEvent');
-        cy.get('[data-cy=timespan]').click().type('24-01-2020');
-        cy.get('[data-cy=description]').click().type('Schuhe mitbringen');
-        cy.get('[data-cy=addEventBtn2]').click();
-        cy.url().should('eq', 'http://localhost:8080/planner');
+        createEvent();
+        deleteEvent();
+    });
 
+    it('addEventAsTrainerSoMemberCanSeeItThenDelete', function () {
+        createEvent();
+        cy.contains('Sign Out').click();
+        cy.url().should('contain', 'login');
+        loginAsMember();
         cy.get('div#cal').contains('Freitagstraining');
-
-        cy.get('[data-cy=deleteEventBtn]').click();
-        cy.url().should('contain', 'delete_event');
-        cy.get('[data-cy=deleteEventName]').click().type('Freitagstraining')
-        cy.get('[data-cy=deleteEventBtn2').click();
-
-        cy.url().should('eq', 'http://localhost:8080/planner');
+        cy.contains('Sign Out').click();
+        loginAsTrainer();
+        deleteEvent();
     });
 });
--- cypress/integration/registrationToLogout.js
+++
@@ -1,7 +0,0 @@
-describe('Registration', function() {
-    it('registration', function () {
-        cy.visit('localhost:8080');
-        cy.url().should('eq', 'http://localhost:8080/login');
-        //cy.contains()
-    })
-});
